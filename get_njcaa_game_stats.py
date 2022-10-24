ecab882a130ffb2b38312b8cd2f8fcce5f0fba44
Joseph Armstrong <armstjc@mail.uc.edu>
2022-10-24 16:25:56 -0400

    10/23/2022 Stats Update 3

--- get_njcaa_game_stats.py
+++ get_njcaa_game_stats.py
@@ -22,7 +22,7 @@ def getNjcaaD1BattingGamelogs(webdriverPath="./chromedriver_mac64_m1"):
 	player_count = len(player_urls)
 	driver = webdriver.Chrome(
 		executable_path=webdriverPath)
-	count = 19500
+	count = 28700
 	for i in tqdm(range(count,player_count)):
 		count += 1
 		print(f'{count}/{player_count}')
