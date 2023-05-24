61d070108683044e5a8459921b86a66b63de6704
Joseph Armstrong <armstjc@mail.uc.edu>
2023-05-24 00:43:32 -0400

    5/24/2023 Stats Update

--- get_njcaa_game_stats.py
+++ get_njcaa_game_stats.py
@@ -113,7 +113,7 @@ def getNjcaaGamelogs(teams_df: pd.DataFrame()):
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
 
         try:
-            response = requests.get(url, headers=headers)
+            response = requests.get(url, headers=headers,timeout=15)
             soup = BeautifulSoup(response.text, features='lxml')
         except:
             print('Coulnd not retrive the webpage')
